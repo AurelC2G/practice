ba638e8dfd53044b4b420fe78f28a2a334ab6350
Aurélien Fredouelle <aurelien.fredouelle@gmail.com>
2013-05-15 12:12:46 -0700

    Iterative (and much faster) version of GCJ 2013 1C-A
    Solves both inputs.

--- googlecodejam/2013/1C/A.py
+++ googlecodejam/2013/1C/A.py
@@ -1,35 +1,9 @@
 import sys
 
-sys.setrecursionlimit(1000)
+sys.setrecursionlimit(100000)
 
 def isConsonnant(c):
     return c not in ['a', 'e', 'i', 'o', 'u']
-
-# [a, b] are the bounds for this search
-def solve(a, b):
-    # Look for a chunk of the right size within those bounds
-    for i in range(a, b - n + 2):
-        if chunks[i] < n:
-            continue
-
-        # we got one. add all the substrings that hold it
-        # start in [a, i]
-        # end in [i+n-1, b]
-        res = (i - a + 1) * (b - (i+n-1) + 1)
-
-        # remaining options don't contain this full chunk:
-
-        # either they start later
-        res += solve(i+1, b)
-
-        # or they end earlier
-        res += solve(a, i+n-2)
-        
-        return res
-
-    # no chunk is long enough in this substring
-    return 0
-
 
 
 T = int(raw_input())
@@ -46,5 +20,37 @@ for testId in range(T):
         if isConsonnant(S[i]):
             chunks[i] = 1 + chunks[i+1]
 
-    res = solve(0, L-1)
+    res = 0
+    a = 0
+    keepLooking = True
+    
+    # [a, L-1] are the bounds for this search
+    while keepLooking:
+        keepLooking = False
+
+        # Look for a chunk of the right size within those bounds
+        for i in range(a, L-1 - n + 2):
+            if chunks[i] < n:
+                continue
+ 
+            # we got one. add all the substrings that hold it
+
+            # first, the ones that start before
+            # start in [a, i]
+            # end in [i+n-1, L-1]
+            res += (i - a + 1) * (L-1 - (i+n-1) + 1)
+
+            # then, the ones that start in the middle of this chunk
+            # start in [i+1, i + chunks[i] - n]
+            # end in [start + n, L-1]
+            # => SUM(L-1 - (start + n) + 2)
+            res += (chunks[i] - n) * (L-1 + 2 - n) - (((i+chunks[i]-n)*(i+chunks[i]-n+1)/2) - (i*(i+1)/2))
+
+            # remaining options don't contain this full chunk. Because we went through chunks in
+            # ascending order, we know we don't have valid substring that contain chunks before
+            # this one, hence they cannot end earlier, and have to start later:
+            a = i + chunks[i] - n + 1
+            keepLooking = True
+            break
+
     print "Case #{:d}: {:d}".format(testId+1, res)
