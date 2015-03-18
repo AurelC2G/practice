24b346771947bda7a87c6aeeced943eafd85e6e6
Aurélien Fredouelle <aurelien.fredouelle@gmail.com>
2015-03-18 14:47:32 +0800

    Added a helper to perform modular exponentiation

--- googlecodejam/template.py
+++ googlecodejam/template.py
@@ -26,6 +26,15 @@ class memoized(object):
     def clear(self):
         self.cache = {}
 
+def modPow(b, e, mod):
+    res = 1
+    b = b % mod
+    while e > 0:
+        if e%2 == 1:
+            res = (res * b) % mod
+        e = e // 2
+        b = (b * b) % mod
+    return res
 
 
 
