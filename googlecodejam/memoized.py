101e1ce7f002568b00a5a0d460f85fda5667ca87
Aurélien Fredouelle <aurelien.fredouelle@gmail.com>
2019-04-28 16:59:00 +0100

    Moved util functions out of the template

---
+++ googlecodejam/memoized.py
@@ -0,0 +1,19 @@
+class memoized(object):
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+    def __call__(self, *args):
+        if args in self.cache:
+            return self.cache[args]
+        else:
+            value = self.func(*args)
+            self.cache[args] = value
+            return value
+    def __repr__(self):
+        '''Return the function's docstring.'''
+        return self.func.__doc__
+    def __get__(self, obj, objtype):
+        '''Support instance methods.'''
+        return functools.partial(self.__call__, obj)
+    def clear(self):
+        self.cache = {}
