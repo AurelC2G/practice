c0d2f87500aa8f1d38d0fdec025c48aec5cf2388
Aurélien Fredouelle <aurelien.fredouelle@gmail.com>
2019-05-04 09:51:01 +0100

    Fixed GCJ 2019 1B: Draupnir

--- googlecodejam/2019/1B/Draupnir/code.py
+++ googlecodejam/2019/1B/Draupnir/code.py
@@ -3,8 +3,7 @@ import itertools
 import math
 import collections
 import functools
-
-sys.setrecursionlimit(10000)
+import numpy
 
 def inputInts():
     return map(int, raw_input().split())
@@ -12,64 +11,35 @@ def inputInts():
 # For easy case, W=6 so we can just try in the first 6 days and get the answer
 # For hard case. W=2. What to do?
 
-def getAnswer():
+def getAnswer(n):
+    print n
+    sys.stdout.flush()
     ans = int(raw_input())
     if ans == -1:
         sys.exit(1)
     return ans
 
+a = numpy.array([
+    [2, 1, 1, 1, 1, 1],
+    [4, 2, 1, 1, 1, 1],
+    [8, 2, 2, 1, 1, 1],
+    [16, 4, 2, 2, 1, 1],
+    [32, 4, 2, 2, 2, 1],
+    [64, 8, 4, 2, 2, 2],
+])
+
 T, W = inputInts()
 for testId in range(T):
-    print 7
+    res = map(int, numpy.linalg.solve(a, numpy.array([
+        getAnswer(1),
+        getAnswer(2),
+        getAnswer(3),
+        getAnswer(4),
+        getAnswer(5),
+        getAnswer(6),
+    ])))
+
+    print " ".join(map(str, res))
     sys.stdout.flush()
-    A7 = getAnswer()
-
-    print 6
-    sys.stdout.flush()
-    A6 = getAnswer()
-    R1 = int((A7 - A6) / 64)
-
-    print 5
-    sys.stdout.flush()
-    A5 = getAnswer()
-
-    print 4
-    sys.stdout.flush()
-    A4 = getAnswer()
-    R5 = A5 - A4 - 16 * R1
-
-
-    print 3
-    sys.stdout.flush()
-    A3 = getAnswer()
-
-    print 2
-    sys.stdout.flush()
-    A2 = getAnswer()
-    R3 = A3 - A2 - 4 * R1
-
-    # 1: 2 1 1 1 1 1
-    # 2: 4 2 1 1 1 1
-    # 3: 8 2 2 1 1 1
-    # 4: 16 4 2 2 1 1
-    # 5: 32 4 2 2 2 1
-    # 6: 64 8 4 2 2 2
-    # 7: 128 8 4 2 2 2
-
-    A6 - A5 = 32 R1 - 4 R2 - 2 R3 - R6
-
-
-    A4 - A2 - 12 R1 - R3 = 2 R2 +R4
-
-    A7 - A5 = xxx R1 + 4 R2 + 2 R3 + R6
-    
-    
-    R2 = A6-A5-R1-R3
-    
-    R4 = A4-A3-R1-R2
-
-    R6 = A2-3*R1-2*R2 -R3-R4-R5
-
-    print " ".join(map(str, [R1, R2, R3, R4, R5, R6]))
-    sys.stdout.flush()
-    res = getAnswer()
+    if int(raw_input()) == -1:
+        sys.exit(1)
